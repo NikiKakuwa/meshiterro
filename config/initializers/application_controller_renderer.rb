c5cc26c932171a00f8c6bec86c05a740be058e83
Niki Kakuwa <haru_knk@yahoo.co.jp>
2020-02-13 21:15:10 +0900

    [Finish]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '878fa4f797669614842400f03d97486be0966e44b332f823e2bc7ffb979788f810c3d1bb37e4d70ea26664d220e111df17ddc9c5fc3e715abbfe2ea18c883ea8'
